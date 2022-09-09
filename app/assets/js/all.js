c219366a88e50c737eff582830e29629e9c892a4
呂向煬 <106233747+lynickyang@users.noreply.github.com>
2022-09-09 17:27:10 +0800

    Update 2022-09-09T09:27:07.640Z

--- app/assets/js/all.js
+++
@@ -1,5 +0,0 @@
-const swiper = new Swiper(".swiper-program", {
-  slidesPerView: "auto",
-  spaceBetween: 8,
-  grabCursor: true,
-});
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var swiper = new Swiper(".swiper-program", {
+  slidesPerView: "auto",
+  spaceBetween: 8,
+  grabCursor: true
+});
+//# sourceMappingURL=all.js.map
